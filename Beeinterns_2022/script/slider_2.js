7db57faf4197513ce4cda4f112300f9c004b3d66
TheZnat <TheZnat7@yandex.ru>
2022-04-15 02:45:08 +0300

    fix bug

---
+++ Beeinterns_2022/script/slider_2.js
@@ -0,0 +1,18 @@
+let counter = 0;
+const sliderLineSecond = document.querySelector('.slider-line-2');
+
+document.querySelector('.slider-next-2').addEventListener('click',  () => {
+    counter += 270;
+    if(counter > 1080){
+        counter = 0;
+    }
+    sliderLineSecond.style.left = -counter + 'px';
+})
+
+document.querySelector('.slider-prev-2').addEventListener('click',  () => {
+    counter -= 270;
+    if( counter < 0){
+        counter = 1080;
+    }
+    sliderLineSecond.style.left = - counter + 'px';
+})
---
+++ Beeinterns_2022/test/switch.js
@@ -0,0 +1,26 @@
+let switches = document.querySelectorAll('.switch');
+
+// for (let i = 0; i < switches.length; i++) {
+//     switches[i].addEventListener('click', function(event) {
+//         if (this.classList.contains('active')) {
+//             this.classList.remove('active');
+//             this.querySelector('input[type=checkbox]').checked = false
+//         } else {
+//
+//             this.classList.add('active');
+//             this.querySelector('input[type=checkbox]').checked = true
+//         }
+//     })
+// }
+
+for (let i = 0; i < switches.length; i++) {
+    switches[i].addEventListener('click', function(event) {
+        if (this.classList.contains('switch-active')) {
+            this.classList.remove('switch-active');
+            this.querySelector('input[type=checkbox]').checked = false
+        } else {
+            this.classList.add('switch-active');
+            this.querySelector('input[type=checkbox]').checked = true
+        }
+    })
+}
